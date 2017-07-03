d860f1b6849b51cdf99eccec1be33766d45311a3
Roman Heinrich <roman.heinrich@gmail.com>
2017-07-03 21:45:53 +0200

    decline friendship cmd

--- lib/twitter/ops/user/cmd_decline_friendship_request.rb
+++ lib/twitter/ops/user/cmd_decline_friendship_request.rb
@@ -2,19 +2,58 @@ module Ops
   module User
     class CmdDeclineFriendshipRequest < Util::Command
       class Form < Util::Form
-        attr_accessor :friend_
-        attr_accessor :password
-        attr_accessor :password_confirmation
-
+        attr_accessor :from
         def validate
-          assert_present :email
-          assert_email :email
-          assert_present :password
-          assert_length :password, 3..32
-          assert password == password_confirmation, %i[password confirmation_does_not_match]
+          assert_present :from
+          assert_numeric :from
         end
       end
-      def call; end
+
+      def call
+        return broadcast(:unauthorized)              unless authorized?
+        return broadcast(:invalid_form, form.errors) unless form.valid?
+        return broadcast(:not_found)                 unless from_user
+        return broadcast(:friendship_invalid)        unless friendship
+        decline_friendship
+        broadcast(:ok)
+      end
+
+      private
+
+      def decline_friendship
+        leg1 = Repo::Friendship.find(status: 0, from_id: from_user.id, to_id: current_user_id)
+        leg2 = Repo::Friendship.find(status: 0, from_id: current_user_id, to_id: from_user.id)
+        transaction do
+          leg1.status = :declined
+          leg2.status = :declined
+          leg1.save
+          leg2.save
+        end
+      end
+
+      def authorized?
+        policy.valid?
+      end
+
+      def policy
+        @policy ||= Policy::ValidUserPolicy.new(session.current_user)
+      end
+
+      def friendship
+        @friendship ||= Repo::Friendship.find(status: 0, from_id: from_user.id, to_id: current_user_id)
+      end
+
+      def form
+        @form ||= Form.new(params)
+      end
+
+      def from_user
+        @from_user ||= Repo::User.find(id: form.from)
+      end
+
+      def current_user_id
+        session.current_user.id
+      end
     end
   end
 end
--- test/flows/friendship_test.rb
+++ test/flows/friendship_test.rb
@@ -1,49 +1,82 @@
 require 'test_helper'
 
-describe 'testing friendship requests' do
+describe "Frienship flows" do
   let(:system) { System.new }
-  it do
+  before(:all){
     system.cmd('cmd.auth.signup', email: 'user1@example.com', password: 'password123', password_confirmation: 'password123')
     system.cmd('cmd.auth.signup', email: 'user2@example.com', password: 'password456', password_confirmation: 'password456')
     system.cmd('cmd.auth.signup', email: 'user3@example.com', password: 'password789', password_confirmation: 'password789')
     user1 = system.qry('qry.user.by_email', email: 'user1@example.com')
     user2 = system.qry('qry.user.by_email', email: 'user2@example.com')
     user3 = system.qry('qry.user.by_email', email: 'user3@example.com')
-
     system.cmd('cmd.auth.confirm_email', token: user1.confirm_token)
     system.cmd('cmd.auth.confirm_email', token: user2.confirm_token)
     system.cmd('cmd.auth.confirm_email', token: user3.confirm_token)
-    [user1, user2, user3].each(&:reload) # update user instances from db
+  }
+  describe 'signup + confirm + send friend request + accept friend request' do
+    it do
+      user1 = system.qry('qry.user.by_email', email: 'user1@example.com')
+      user2 = system.qry('qry.user.by_email', email: 'user2@example.com')
+      user3 = system.qry('qry.user.by_email', email: 'user3@example.com')
+      system.with_user(user1) do
+        assert_empty system.qry('qry.user.all_friends')
+        system.cmd('cmd.user.send_friendship_request', to: user2.id)
+        system.cmd('cmd.user.send_friendship_request', to: user3.id)
+        assert_empty system.qry('qry.user.all_friends')
+      end
 
-    system.with_user(user1) do
-      assert_empty system.qry('qry.user.all_friends')
-      system.cmd('cmd.user.send_friendship_request', to: user2.id)
-      system.cmd('cmd.user.send_friendship_request', to: user3.id)
-      assert_empty system.qry('qry.user.all_friends')
+      system.with_user(user2) do
+        assert_empty system.qry('qry.user.all_friends')
+        system.cmd('cmd.user.accept_friendship_request', from: user1.id)
+        assert_equal system.qry('qry.user.all_friends').map(&:id), [user1].map(&:id)
+      end
+
+      system.with_user(user1) do
+        friends = system.qry('qry.user.all_friends')
+        assert_equal friends.map(&:id), [user2].map(&:id)
+      end
+
+      system.with_user(user3) do
+        assert_empty system.qry('qry.user.all_friends')
+        system.cmd('cmd.user.accept_friendship_request', from: user1.id)
+        friends = system.qry('qry.user.all_friends')
+        assert_equal friends.map(&:id), [user1].map(&:id)
+      end
+
+      system.with_user(user1) do
+        friends = system.qry('qry.user.all_friends')
+        refute_equal friends.map(&:id), [user2].map(&:id)
+        assert_equal friends.map(&:id), [user2, user3].map(&:id)
+      end
     end
+  end
 
-    system.with_user(user2) do
-      assert_empty system.qry('qry.user.all_friends')
-      system.cmd('cmd.user.accept_friendship_request', from: user1.id)
-      assert_equal system.qry('qry.user.all_friends').map(&:id), [user1].map(&:id)
-    end
-
-    system.with_user(user1) do
-      friends = system.qry('qry.user.all_friends')
-      assert_equal friends.map(&:id), [user2].map(&:id)
-    end
-
-    system.with_user(user3) do
-      assert_empty system.qry('qry.user.all_friends')
-      system.cmd('cmd.user.accept_friendship_request', from: user1.id)
-      friends = system.qry('qry.user.all_friends')
-      assert_equal friends.map(&:id), [user1].map(&:id)
-    end
-
-    system.with_user(user1) do
-      friends = system.qry('qry.user.all_friends')
-      refute_equal friends.map(&:id), [user2].map(&:id)
-      assert_equal friends.map(&:id), [user2, user3].map(&:id)
+  describe 'declining friendship requests' do
+    it do
+      user1 = system.qry('qry.user.by_email', email: 'user1@example.com')
+      user2 = system.qry('qry.user.by_email', email: 'user2@example.com')
+      user3 = system.qry('qry.user.by_email', email: 'user3@example.com')
+      system.with_user(user1) do
+        assert_empty system.qry('qry.user.all_friends')
+        system.cmd('cmd.user.send_friendship_request', to: user2.id)
+        system.cmd('cmd.user.send_friendship_request', to: user3.id)
+        assert_empty system.qry('qry.user.all_friends')
+      end
+      system.with_user(user2) do
+        assert_empty system.qry('qry.user.all_friends')
+        system.cmd('cmd.user.decline_friendship_request',from: user1.id)
+        assert_empty system.qry('qry.user.all_friends')
+      end
+      system.with_user(user3) do
+        system.cmd('cmd.user.accept_friendship_request', from: user1.id)
+        assert_equal system.qry('qry.user.all_friends').map(&:id), [user1.id]
+      end
+      system.with_user(user1) do
+        assert_equal system.qry('qry.user.all_friends').map(&:id), [user3.id]
+      end
     end
   end
 end
+
+
+
