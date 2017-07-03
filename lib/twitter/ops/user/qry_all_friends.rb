f5c4f9f7f1d6fac77e30ffef3cf77246665a3317
Roman Heinrich <roman.heinrich@gmail.com>
2017-07-03 21:46:17 +0200

    some improvements for qry_all_friends

--- lib/twitter/ops/user/qry_all_friends.rb
+++ lib/twitter/ops/user/qry_all_friends.rb
@@ -3,29 +3,27 @@ module Ops
     class QryAllFriends < Util::Query
       class Form < Util::Form
         attr_accessor :page, :per_page, :status
-        STATUS_MAP = {
-          pending: 0,
-          accepted: 1,
-          declined: 2,
-          blocked: 3
-        }.freeze
-
         def validate
           assert_numeric :page if page
-          page ||= 1
+          self.page ||= 1
 
           assert_numeric :per_page if per_page
-          per_page ||= 15
+          self.per_page ||= 15
 
           assert_member :status, %w[pending accepted declined blocked] if status
+          self.status ||= 'accepted'
         end
 
         def to_params
           {
             page: page,
             per_page: per_page,
-            status: STATUS_MAP[status.to_sym]
+            status: status_map(status)
           }
+        end
+
+        def status_map(status)
+          Util::Enum::FRIENDSHIP_STATUS[status.to_sym]
         end
       end
 
